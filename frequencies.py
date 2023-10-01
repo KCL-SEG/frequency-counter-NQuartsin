8e87db00ca2f41dcc2b20f00fd02b9f2fec17332
Naomi Quartsin <naomi.quartsin@kcl.ac.uk>
2023-10-02 00:50:00 +0100

    Add code to frequencies.py

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,16 @@
 
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+    i = 0
+    j = 1
+        while(i<len(items)):
+            if not items[i] in frequencies:
+                counter = 1
+                while(j<len(items)):
+                    if (items[i] == items[j]):
+                        counter = counter + 1
+                    j = j + 1
+                frequencies[items[i]] = counter
+            i = i + 1
+            j = i + 1
     return frequencies
