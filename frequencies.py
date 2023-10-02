d2945b18c9419f3a056a09796de96c18f1ae7ed2
Naomi Quartsin <naomi.quartsin@kcl.ac.uk>
2023-10-03 00:34:27 +0100

    Correct code to calculate frequencies correctly

--- frequencies.py
+++ frequencies.py
@@ -4,12 +4,13 @@
 def frequencies(items):
     frequencies = {}
     i = 0
+    items = [str(item).strip(" '") for item in items]
     while(i<len(items)):
         if items[i] not in frequencies:
             counter = 1
             j = i + 1
             while(j<len(items)):
-                if (items[i] == items[j]):
+                if (str(items[i] == items[j])):
                     counter = counter + 1
                 j = j + 1
             frequencies[items[i]] = counter
