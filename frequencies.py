b4025b07aa10e9f834723191821a9d564f942ef3
Naomi Quartsin <naomi.quartsin@kcl.ac.uk>
2023-10-02 23:22:34 +0100

    Fix indentation and logic error

--- frequencies.py
+++ frequencies.py
@@ -4,15 +4,14 @@
 def frequencies(items):
     frequencies = {}
     i = 0
-    j = 1
-        while(i<len(items)):
-            if not items[i] in frequencies:
-                counter = 1
-                while(j<len(items)):
-                    if (items[i] == items[j]):
-                        counter = counter + 1
-                    j = j + 1
-                frequencies[items[i]] = counter
-            i = i + 1
+    while(i<len(items)):
+        if items[i] not in frequencies:
+            counter = 1
             j = i + 1
+            while(j<len(items)):
+                if (items[i] == items[j]):
+                    counter = counter + 1
+                j = j + 1
+            frequencies[items[i]] = counter
+        i = i + 1
     return frequencies
